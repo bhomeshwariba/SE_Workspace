dcac6e889584d63a2419f5029aacf52c52183a8d
bhomeshwariba <bhomeshwaribasolanki@gmail.com>
2026-06-29 18:05:35 +0530

    commit

---
+++ module2/task2.1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n my name is bhomeshwariba");
+    printf("\n my birth date is 4/12/2005");
+    printf("\n my age is 21");
+    printf("\n my address from kodinar");
+    getch();
+}
---
+++ module2/task2.2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a = 10;
+    int b = 5;
+
+    int add = a+b;
+    printf("\n add of two value is: %d", add);
+
+    int sub = a-b;
+    printf("\n sub of two value is: %d", sub);
+
+    int mul = a*b;
+    printf("\n mul of two value is: %d", mul);
+
+    int division = a/b;
+    printf("\n division of two value is: %d", division);
+
+    int mod = a%b;
+    printf("\n mod of two value is: %d", mod);
+
+    getch();
+}
