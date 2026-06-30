223c28a007e48db260d4806b1d48ce855d853a5b
bhomeshwariba <bhomeshwaribasolanki@gmail.com>
2026-06-30 09:48:52 +0530

    commit

---
+++ module2/task2.5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int b=20;
+    int h=13;
+    
+    
+    float area=0.10*b*h;
+    printf("area of triangle is: %f",area);
+    getch();
+}
